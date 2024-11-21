736fba50c6f64c54d039ae846f90d0812dda0c24
Saufik Ramadhan <saufik.ramadhan@ecomindo.com>
2024-11-22 00:17:06 +0700

    fix: menu child oled, testing IR send-receive with known protocols

---
+++ src/firmware_integrated/Menu_List.h
@@ -0,0 +1,13 @@
+// MENU ENUMS
+#define MENU_IR_MODULE            1
+#define MENU_IR_MODULE_LENGTH     3
+#define MENU_IR_MODULE__READ      11
+#define MENU_IR_MODULE__WRITE     12
+#define MENU_IR_MODULE__LIST      13
+
+#define MENU_NFC_MODULE           2
+#define MENU_WIFI_MODULE          3
+#define MENU_SD_MODULE            4
+#define MENU_BLUETOOTH_MODULE     5
+#define MENU_EXIT                 6
+#define MENU_CONFIRMATION         7
